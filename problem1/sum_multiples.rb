dc4af484cd6254156fa2026c371252a388ba71a7
Tom Watts <github@tomwatts.ca>
2014-09-13 00:06:23 -0400

    Adding fully functional solution.

--- problem1/sum_multiples.rb
+++ problem1/sum_multiples.rb
@@ -2,15 +2,31 @@
 
 # Gets the sum of all multiples up to a given limit of numbers in a given array
 # * *Args*    :
-#   - +multiples+ -> 
+#   - +numbers+ -> 
 #   - +limit+ -> 
 # * *Returns* :
 #   - The sum of all multiples of the numbers of the given array
 
-def sum_multiples(multiples, limit)
+def sum_multiples(numbers, limit)
 	sum = 0
+	(numbers[0]..limit-1).each { |potential_multiple|
+		i = 0
+		has_been_counted = false
+		while (i < numbers.length && !has_been_counted) do
+			if (potential_multiple % numbers[i] == 0)
+				has_been_counted = true
+				sum += potential_multiple
+			end
+			i += 1
+		end
+	}
+
 	return sum
 end
 
-print sum_multiples([3, 5], 10).to_s + "\n"
+numbers = [3, 5]
+limit = 1000
+
+print "Sum of all multiples of " + numbers.join(", ") + " below " + limit.to_s +
+	" is " + sum_multiples(numbers, limit).to_s + "\n"
 
