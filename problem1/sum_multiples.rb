c6d1bf8e39e3495012e0e65ce24fc6d49b0ab6e8
Tom Watts <github@tomwatts.ca>
2014-09-12 22:59:39 -0400

    Adding partial solution; could include multiples in sum more than once.

---
+++ problem1/sum_multiples.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# Gets the sum of all multiples up to a given limit of numbers in a given array
+# * *Args*    :
+#   - +multiples+ -> 
+#   - +limit+ -> 
+# * *Returns* :
+#   - The sum of all multiples of the numbers of the given array
+
+def sum_multiples(multiples, limit)
+	sum = 0
+	return sum
+end
+
+print sum_multiples([3, 5], 10).to_s + "\n"
+
