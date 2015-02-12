355a8205f9b906eca38eb90afe66abcd489b9e58
tomwatts <github@tomwatts.ca>
2015-02-12 15:16:10 -0500

    Refactoring and removing extraneous messages.

--- problem59/decipher.rb
+++ problem59/decipher.rb
@@ -7,30 +7,24 @@ def decipher_file(encoded_file)
 	File.new("words").each().each do |line|
 		words[line.chop] = nil
 	end
-	print "done!\n"
+	puts "done!"
 
 	text = File.new(encoded_file).read()
 	tokens = text.split(",")
 	decoded_message = ""
-	decoded_message_sum = 0
 
-	('aaa'..'zzz').each do |key|
-	#('aaa'..'aad').each do |key|
-		#print "Current key: #{key}\n"
+	("aaa".."zzz").each do |key|
 		decoded_message = ""
 		i = 0
 		tokens.each do |token|
-			#print "#{key[i].ord} ^ #{token.to_i} = #{key[i].ord ^ token.to_i}\n"
 			decoded_token = key[i].ord ^ token.to_i
 			decoded_message += decoded_token.chr
-			decoded_message_sum += decoded_token.to_i
-			#if (decoded_token == " ")
-				# TODO: check to see if message so far is english
-			#end
 			i = (i + 1) % key.length()
 		end
 
-		return decoded_message if (is_english(decoded_message, words, 50))
+		if (is_english(decoded_message, words, 90))
+			return decoded_message 
+		end
 	end
 
 	return nil
@@ -47,32 +41,40 @@ def is_english(text, english_dictionary, threshold)
 		n_words += 1
 		if (english_dictionary.has_key?(token))
 			n_english_words += 1
-		else
-			print "#{token} is not in the english dictionary\n"
 		end
 	end
 
 	percent_english = 100 * n_english_words / n_words.to_f
 	if (percent_english > threshold)
-		print "percent_english: #{percent_english}\n"
-		print "text: #{text}\n"
 		return true
 	end
 
 	return false
 end
 
+def sum_string(text)
+	sum = 0
+
+	text.each_char do |c|
+		sum += c.ord
+	end
+
+	return sum
+end
+
 file_name = ARGV[0]
 if (file_name.nil?)
-	abort "Usage: #{$0} FILENAME\n"
+	abort "Usage: #{$0} FILENAME"
 elsif (!File.exist?(file_name))
-	abort "#{file_name} does not exist\n"
+	abort "#{file_name} does not exist"
 end
 
 start_time = Time.now
-result = decipher_file(file_name)
+decoded_message = decipher_file(file_name)
+decoded_message_sum = sum_string(decoded_message)
 end_time = Time.now
 
-print "Elapsed time: #{end_time - start_time} s\n"
-print "Decoded message: #{result}\n"
+puts "Decoded message: #{decoded_message}"
+puts "Decoded message sum: #{decoded_message_sum}"
+puts "Elapsed time: #{end_time - start_time} s"
 
